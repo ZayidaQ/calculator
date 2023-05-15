c808862f7c265a6a05741049fa7c7d178cd86cad
Zhayida Haishan <zahideqeyser01@gmail.com>
2023-05-15 14:10:56 -0700

    update

--- js/index.js
+++ js/index.js
@@ -19,62 +19,82 @@ function init(){
 }
 
 function onAddBtnClicked(){
+    //find HTML elements
     const number1Field = Number(document.getElementById("number1Field").value);
     const number2Field = Number(document.getElementById("number2Field").value);
     const messagePara = document.getElementById("messagePara");
-     
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "Input is not a number!";
-        return;
-    }
-    const result = number1Field + number2Field;
-    document.getElementById("answerField").value = result;
 
+    // check to see of user inputs were invalid
+    if (isNaN(number1Field) || isNaN(number2Field)) {
+        messagePara.innerHTML = "One or more of your input values are invalid";
+        return; // exit the addBtnClicked function
+    }
+
+    const answer = number1Field + number2Field;
+    const answerField = document.getElementById("answerField");
+    answerField.value = answer;
+
+    // clear any previous error message
     messagePara.innerHTML = "";
 }
 
 function onSubtractBtnClicked(){
+    //find HTML elements
     const number1Field = Number(document.getElementById("number1Field").value);
     const number2Field = Number(document.getElementById("number2Field").value);
     const messagePara = document.getElementById("messagePara");
-     
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "Input is not a number!";
-        return;
-    }
-    const result = number1Field - number2Field;
-    document.getElementById("answerField").value = result;
 
+    // check to see of user inputs were invalid
+    if (isNaN(number1Field) || isNaN(number2Field)) {
+        messagePara.innerHTML = "One or more of your input values are invalid";
+        return; // exit the addBtnClicked function
+    }
+
+    const answer = number1Field - number2Field;
+    const answerField = document.getElementById("answerField");
+    answerField.value = answer;
+
+    // clear any previous error message
     messagePara.innerHTML = "";
 }
 
 function onMultiplyBtnClicked(){
+    //find HTML elements
     const number1Field = Number(document.getElementById("number1Field").value);
     const number2Field = Number(document.getElementById("number2Field").value);
     const messagePara = document.getElementById("messagePara");
-     
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "Input is not a number!";
-        return;
-    }
-    const result = number1Field * number2Field;
-    document.getElementById("answerField").value = result;
 
+    // check to see of user inputs were invalid
+    if (isNaN(number1Field) || isNaN(number2Field)) {
+        messagePara.innerHTML = "One or more of your input values are invalid";
+        return; // exit the addBtnClicked function
+    }
+
+    const answer = number1Field * number2Field;
+    const answerField = document.getElementById("answerField");
+    answerField.value = answer;
+
+    // clear any previous error message
     messagePara.innerHTML = "";
 }
 
 function onDivideBtnClicked(){
+    //find HTML elements
     const number1Field = Number(document.getElementById("number1Field").value);
     const number2Field = Number(document.getElementById("number2Field").value);
     const messagePara = document.getElementById("messagePara");
-     
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "Input is not a number!";
-        return;
-    }
-    const result = number1Field * number2Field;
-    document.getElementById("answerField").value = result;
 
+    // check to see of user inputs were invalid
+    if (isNaN(number1Field) || isNaN(number2Field)) {
+        messagePara.innerHTML = "One or more of your input values are invalid";
+        return; // exit the addBtnClicked function
+    }
+
+    const answer = number1Field / number2Field;
+    const answerField = document.getElementById("answerField");
+    answerField.value = answer;
+
+    // clear any previous error message
     messagePara.innerHTML = "";
 }
 
