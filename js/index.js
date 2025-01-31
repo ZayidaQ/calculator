eff6bfa842c6317817b6f7e5b9e0348e2ffb60b1
ZayidaQ <zahideqeyser01@gmail.com>
2025-01-31 15:48:11 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,105 +1,105 @@
 //Author: Zhayida Haishan
 
-"use sprict";
+"use strict";
 
 window.onload = init;
 
-function init(){
-    const addBtn = document.getElementById("addBtn");
-    const subtractBtn = document.getElementById("subBtn");
-    const multiplyBtn = document.getElementById("multBtn");
-    const divideBtn = document.getElementById("divBtn");
-    const clearBtn = document.getElementById("clearBtn");
+function init() {
+  const addBtn = document.getElementById("addBtn");
+  const subtractBtn = document.getElementById("subBtn");
+  const multiplyBtn = document.getElementById("multBtn");
+  const divideBtn = document.getElementById("divBtn");
+  const clearBtn = document.getElementById("clearBtn");
 
-    addBtn.onclick = onAddBtnClicked;
-    subtractBtn.onclick = onSubtractBtnClicked;
-    multiplyBtn.onclick = onMultiplyBtnClicked;
-    divideBtn.onclick = onDivideBtnClicked;
-    clearBtn.onclick = onClearBtnClicked;
+  addBtn.onclick = onAddBtnClicked;
+  subtractBtn.onclick = onSubtractBtnClicked;
+  multiplyBtn.onclick = onMultiplyBtnClicked;
+  divideBtn.onclick = onDivideBtnClicked;
+  clearBtn.onclick = onClearBtnClicked;
 }
 
-function onAddBtnClicked(){
-    //find HTML elements
-    const number1Field = Number(document.getElementById("number1Field").value);
-    const number2Field = Number(document.getElementById("number2Field").value);
-    const messagePara = document.getElementById("messagePara");
+function onAddBtnClicked() {
+  //find HTML elements
+  const number1Field = Number(document.getElementById("number1Field").value);
+  const number2Field = Number(document.getElementById("number2Field").value);
+  const messagePara = document.getElementById("messagePara");
 
-    // check to see of user inputs were invalid
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "One or more of your input values are invalid";
-        return; // exit the addBtnClicked function
-    }
+  // check to see of user inputs were invalid
+  if (isNaN(number1Field) || isNaN(number2Field)) {
+    messagePara.innerHTML = "One or more of your input values are invalid";
+    return; // exit the addBtnClicked function
+  }
 
-    const answer = number1Field + number2Field;
-    const answerField = document.getElementById("answerField");
-    answerField.value = answer;
+  const answer = number1Field + number2Field;
+  const answerField = document.getElementById("answerField");
+  answerField.value = answer;
 
-    // clear any previous error message
-    messagePara.innerHTML = "";
+  // clear any previous error message
+  messagePara.innerHTML = "";
 }
 
-function onSubtractBtnClicked(){
-    //find HTML elements
-    const number1Field = Number(document.getElementById("number1Field").value);
-    const number2Field = Number(document.getElementById("number2Field").value);
-    const messagePara = document.getElementById("messagePara");
+function onSubtractBtnClicked() {
+  //find HTML elements
+  const number1Field = Number(document.getElementById("number1Field").value);
+  const number2Field = Number(document.getElementById("number2Field").value);
+  const messagePara = document.getElementById("messagePara");
 
-    // check to see of user inputs were invalid
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "One or more of your input values are invalid";
-        return; // exit the addBtnClicked function
-    }
+  // check to see of user inputs were invalid
+  if (isNaN(number1Field) || isNaN(number2Field)) {
+    messagePara.innerHTML = "One or more of your input values are invalid";
+    return; // exit the addBtnClicked function
+  }
 
-    const answer = number1Field - number2Field;
-    const answerField = document.getElementById("answerField");
-    answerField.value = answer;
+  const answer = number1Field - number2Field;
+  const answerField = document.getElementById("answerField");
+  answerField.value = answer;
 
-    // clear any previous error message
-    messagePara.innerHTML = "";
+  // clear any previous error message
+  messagePara.innerHTML = "";
 }
 
-function onMultiplyBtnClicked(){
-    //find HTML elements
-    const number1Field = Number(document.getElementById("number1Field").value);
-    const number2Field = Number(document.getElementById("number2Field").value);
-    const messagePara = document.getElementById("messagePara");
+function onMultiplyBtnClicked() {
+  //find HTML elements
+  const number1Field = Number(document.getElementById("number1Field").value);
+  const number2Field = Number(document.getElementById("number2Field").value);
+  const messagePara = document.getElementById("messagePara");
 
-    // check to see of user inputs were invalid
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "One or more of your input values are invalid";
-        return; // exit the addBtnClicked function
-    }
+  // check to see of user inputs were invalid
+  if (isNaN(number1Field) || isNaN(number2Field)) {
+    messagePara.innerHTML = "One or more of your input values are invalid";
+    return; // exit the addBtnClicked function
+  }
 
-    const answer = number1Field * number2Field;
-    const answerField = document.getElementById("answerField");
-    answerField.value = answer;
+  const answer = number1Field * number2Field;
+  const answerField = document.getElementById("answerField");
+  answerField.value = answer;
 
-    // clear any previous error message
-    messagePara.innerHTML = "";
+  // clear any previous error message
+  messagePara.innerHTML = "";
 }
 
-function onDivideBtnClicked(){
-    //find HTML elements
-    const number1Field = Number(document.getElementById("number1Field").value);
-    const number2Field = Number(document.getElementById("number2Field").value);
-    const messagePara = document.getElementById("messagePara");
+function onDivideBtnClicked() {
+  //find HTML elements
+  const number1Field = Number(document.getElementById("number1Field").value);
+  const number2Field = Number(document.getElementById("number2Field").value);
+  const messagePara = document.getElementById("messagePara");
 
-    // check to see of user inputs were invalid
-    if (isNaN(number1Field) || isNaN(number2Field)) {
-        messagePara.innerHTML = "One or more of your input values are invalid";
-        return; // exit the addBtnClicked function
-    }
+  // check to see of user inputs were invalid
+  if (isNaN(number1Field) || isNaN(number2Field)) {
+    messagePara.innerHTML = "One or more of your input values are invalid";
+    return; // exit the addBtnClicked function
+  }
 
-    const answer = number1Field / number2Field;
-    const answerField = document.getElementById("answerField");
-    answerField.value = answer;
+  const answer = number1Field / number2Field;
+  const answerField = document.getElementById("answerField");
+  answerField.value = answer;
 
-    // clear any previous error message
-    messagePara.innerHTML = "";
+  // clear any previous error message
+  messagePara.innerHTML = "";
 }
 
 function onClearBtnClicked() {
-    document.getElementById("number1Field").value = "";
-    document.getElementById("number2Field").value = "";
-    document.getElementById("answerField").value = "";
+  document.getElementById("number1Field").value = "";
+  document.getElementById("number2Field").value = "";
+  document.getElementById("answerField").value = "";
 }
